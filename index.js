146e2bac12aa3e662005315f5fa89a1c217d4680
jared <jaredpattison@users.noreply.github.com>
2019-02-28 15:14:28 -0800

    comment out logs

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/App.js';
-
-import createStore from './store/index.js';
-const store = createStore();
 
 function Main() {
   return( 
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <>  
+    <App />
+    </>
   );
 }
 
