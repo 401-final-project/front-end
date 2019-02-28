24cbf8b41f4aba62151e469ba32f7bff4c6c0b6b
rkgallaway <rkgallaway@msn.com>
2019-02-28 10:01:43 -0800

    scanner styled, modal created and added to contact

--- screens/contactScreen.js
+++ screens/contactScreen.js
@@ -5,6 +5,7 @@ import * as Expo from 'expo';
 import { connect } from 'react-redux';
 import {Provider} from 'react-redux';
 import store from '../store';
+import DetailModal from '../components/detailsModal';
 
 const mapStateToProps = state => {
   return ({
@@ -27,6 +28,7 @@ class UserContacts extends React.Component {
             )
           }
         </View>
+        <DetailModal />
 
       </ScrollView>
       </Provider>
--- screens/shareScreen.js
+++ screens/shareScreen.js
@@ -6,13 +6,32 @@ import QR from '../components/qr.js';
 class ShareScreen extends React.Component {
   render() {
     return(
-      <ScrollView>
-        <QR />
+      <ScrollView >
+      <View style={styles.container}>
+        <QR style={styles.qrScanner}/>
         <ShareSelector />
+        </View>
       </ScrollView>
     )
   }
   
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  qrScanner: {
+    width: 300,
+    height: 300,
+  }
+});
+
+// <View style={styles.container}>
+// style={styles.qrScanner}
+
 
 export default ShareScreen;
