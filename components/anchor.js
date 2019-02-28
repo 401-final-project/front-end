ba42a42771048762ec1a2153a1733aba6c7d91a4
rkgallaway <rkgallaway@msn.com>
2019-02-28 15:55:02 -0800

    anchor component

---
+++ components/anchor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Linking, Text } from 'react-native';
+
+class Anchor extends React.Component {
+  _handlePress = () => {
+    Linking.openURL(this.props.href);
+    this.props.onPress && this.props.onPress();
+  };
+
+  render() {
+    return (
+      <Text {...this.props} onPress={this._handlePress}>
+        {this.props.children}
+      </Text>
+    );
+  }
+}
+
+
+export default Anchor;
