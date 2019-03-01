a075c8dba5263af2433c657f8a9333b2692c0fba
Fletcher LaRue <laruefletcher@gmail.com>
2019-02-28 20:25:26 -0800

    fixes little bug

--- screens/shareScreen.js
+++ screens/shareScreen.js
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 50,
+    backgroundColor: '#f2f3f4'
   },
   qrScanner: {
     width: 300,
