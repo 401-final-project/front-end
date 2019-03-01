92824c5ae906b12820b78447d3f343c96935e605
rkgallaway <40313258+rkgallaway@users.noreply.github.com>
2019-02-28 19:52:48 -0800

    Revert "updates links for contact detail page"

--- components/detailsModal.js
+++ components/detailsModal.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import {Modal, Text, TouchableHighlight, TouchableOpacity, Linking, View, ScrollView, Alert, Image, Button } from 'react-native';
+import {Modal, Text, TouchableHighlight, Linking, View, Alert} from 'react-native';
 
 import Anchor from './anchor';
-import * as icons from '../assets/util.js'; 
 
 class DetailModal extends Component {
   constructor(props){
@@ -18,8 +17,11 @@ class DetailModal extends Component {
 
   render() {
     const info = this.props.information;
+
+    // const 
+
     return (
-      <ScrollView style={{marginTop: 22}}>
+      <View style={{marginTop: 22}}>
         <TouchableHighlight
           onPress={() => {
             this.setModalVisible(true);
@@ -32,62 +34,32 @@ class DetailModal extends Component {
           transparent={false}
           visible={this.state.modalVisible}
           onRequestClose={() => {
-            this.setModalVisible(!this.state.modalVisible);
+            Alert.alert('Modal has been closed.');
           }}>
           <View style={{marginTop: 22}}>
             <View>
               <Text style={styles.detailInfo}>{info.name ? `Name: ${info.name}` : null}</Text>
               <Text style={styles.detailInfo}>{info.phone ? `Phone: ${info.phone}` : null}</Text>
               <Text style={styles.detailInfo}>{info.email ? `Email: ${info.email}` : null}</Text>
-              {info.linkedin ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://linked.com/in/${info.linkedin}`}>
-                    <Image style={styles.icon} source={{uri: icons.LINKEDIN_ICON}} />
-                    Linkedin
-                  </Anchor>
-                </View> : null}
-              {info.pinterest ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://pinterest.com/${info.pinterest}`}>
-                    <Image style={styles.icon} source={{uri: icons.PINTEREST_ICON}} />
-                    Pinterest
-                  </Anchor>
-                </View> : null}
-              {info.twitter ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://twitter.com/${info.twitter}`}>
-                    <Image style={styles.icon} source={{uri: icons.TWITTER_ICON}} />
-                    Twitter
-                  </Anchor>
-                </View> : null}
-              {info.facebook ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://facebook.com/${info.facebook}`}>
-                    <Image style={styles.icon} source={{uri: icons.FACEBOOK_ICON}} />
-                    Facebook
-                  </Anchor>
-                </View> : null}
-              {info.github ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://github.com/${info.github}`}>
-                    <Image style={styles.icon} source={{uri: icons.GITHUB_ICON}} />
-                    Github
-                  </Anchor>
-                </View> : null}
-              {info.instagram ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://instagram.com/${info.instagram}`}>
-                    <Image style={styles.icon} source={{uri: icons.INSTAGRAM_ICON}} />
-                    Instagram
-                  </Anchor>
-                </View> : null}
-              {info.snapchat ? 
-                <View style={styles.iconRow}>
-                  <Anchor style={styles.detailInfo} href={`http://snapchat.com/add/${info.snapchat}`}>
-                    <Image style={styles.icon} source={{uri: icons.SNAPCHAT_ICON}} />
-                    Snapchat
-                  </Anchor>
-                </View> : null}
+
+              <Anchor href="http://linkedin.com/in/fletcher-larue">fletcher's linked in</Anchor>
+
+              <Text style={styles.detailInfo}>{info.linkedin ? `Linkedin: ${info.linkedin}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.linkedin ? `linkedin.com/${info.linkedin}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.pinterest ? `Pinterest: ${info.pinterest}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.twitter ? `Twitter: ${info.twitter}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.facebook ? `Facebook: ${info.facebook}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.github ? `Github: ${info.github}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.instagram ? `Instagram: ${info.instagram}` : null}</Text>
+              <Text style={styles.detailInfo}>{info.snapchat ? `Snapchat: ${info.snapchat}` : null}</Text>
+              {/* linkedin/in/username
+              pinterest.com/username
+              twitter.com/username
+              facebook.com/username
+              github.com/username
+              instagram.com/username
+              snapchat.com/add/username */}
+
               <TouchableHighlight
                 onPress={() => {
                   this.setModalVisible(!this.state.modalVisible);
@@ -97,38 +69,15 @@ class DetailModal extends Component {
             </View>
           </View>
         </Modal>
-      </ScrollView>
+
+      </View>
     );
   }
 }
 
 const styles = {
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 50,
-  },
-  qrScanner: {
-    width: 300,
-    height: 300,
-  },
   detailInfo: {
-    height: 40,
-    margin: 0,
-    marginLeft: 20,
-  },
-  icon: {
-    width: 28,
-    height: 28,
-    marginLeft: 10,
-    marginRight: 100,
-  },
-  iconRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-};
-
+    margin: 10
+  }
+}
 export default DetailModal;
